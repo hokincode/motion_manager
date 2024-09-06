3c974363bc18a2bfb0ef37979615af629fe5b10f
hokindeng <denghokin@gmail.com>
2024-09-06 12:52:42 -0400

    print license also work

--- manage_meta.py
+++ manage_meta.py
@@ -38,7 +38,15 @@ class MetaManager:
             print(f"Dataset {dataset_name} created with default metadata.")
         else:
             print(f"Dataset {dataset_name} already exists.")
-
+    def license(self, dataset_name):
+        license_path = os.path.join(self.dataset_dir, dataset_name, 'LICENSE')
+        if os.path.exists(license_path):
+            with open(license_path, 'r') as f:
+                license_info = f.read()
+                print(f"License for {dataset_name}:")
+                print(license_info)
+        else:
+            print(f"License file for dataset {dataset_name} not found.")
 
 def print_usage():
     usage_text = """
@@ -46,6 +54,7 @@ Usage of manage_meta.py:
     python manage_meta.py --list                    List all available datasets
     python manage_meta.py --load dataset_name       Print metadata of a specific dataset by name
     python manage_meta.py --add dataset_name        Add a new dataset with default metadata
+    python manage_meta.py --license dataset_name     Print license information about a specific dataset
 
 Example Usages:
     - List all datasets:
@@ -54,6 +63,8 @@ Example Usages:
         python manage_meta.py --about my_dataset
     - Add a new dataset called 'new_dataset':
         python manage_meta.py --add new_dataset
+    - print license information about a dataset called 'new_dataset':
+        python manage_meta.py --license new_dataset
     """
     print(usage_text)
 
@@ -62,6 +73,7 @@ def main():
     parser.add_argument("--list", action="store_true", help="List all datasets")
     parser.add_argument("--about", type=str, help="Load metadata for a specific dataset")
     parser.add_argument("--add", type=str, help="Add a new dataset with default metadata")
+    parser.add_argument("--license", type=str, help="Print License information about the dataset")
 
     args = parser.parse_args()
 
@@ -73,6 +85,8 @@ def main():
         manager.print_meta_info(args.about)
     elif args.add:
         manager.add_meta_info(args.add)
+    elif args.license:
+        manager.license(args.license)
     else:
         print_usage()
 
